649a50c3a484cce4ff35911f2b6b7fbb4b676173
Trest <144359854+trest100@users.noreply.github.com>
2025-04-06 17:46:57 +0300

    Revert "Woundingport"

--- Content.Shared/Backmen/Surgery/SharedSurgerySystem.cs
+++ Content.Shared/Backmen/Surgery/SharedSurgerySystem.cs
@@ -1,9 +1,6 @@
 using System.Linq;
 using Content.Shared.Backmen.Surgery.Conditions;
 using Content.Shared.Backmen.Surgery.Steps.Parts;
-using Content.Shared.Backmen.Surgery.Traumas.Systems;
-using Content.Shared.Backmen.Surgery.Wounds.Components;
-using Content.Shared.Backmen.Surgery.Wounds.Systems;
 using Content.Shared.Medical.Surgery.Conditions;
 using Content.Shared.Body.Systems;
 using Content.Shared.Medical.Surgery.Steps;
@@ -23,7 +20,6 @@ using Content.Shared.Inventory;
 using Content.Shared.Popups;
 using Content.Shared.Standing;
 using Robust.Shared.Audio.Systems;
-using Robust.Shared.Containers;
 using Robust.Shared.Map;
 using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
@@ -49,8 +45,6 @@ public abstract partial class SharedSurgerySystem : EntitySystem
     [Dependency] private readonly RotateToFaceSystem _rotateToFace = default!;
     [Dependency] private readonly StandingStateSystem _standing = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
-    [Dependency] private readonly WoundSystem _wounds = default!;
-    [Dependency] private readonly TraumaSystem _trauma = default!;
 
     private readonly Dictionary<EntProtoId, EntityUid> _surgeries = new();
 
@@ -69,7 +63,6 @@ public abstract partial class SharedSurgerySystem : EntitySystem
         SubscribeLocalEvent<SurgeryWoundedConditionComponent, SurgeryValidEvent>(OnWoundedValid);
         SubscribeLocalEvent<SurgeryPartRemovedConditionComponent, SurgeryValidEvent>(OnPartRemovedConditionValid);
         SubscribeLocalEvent<SurgeryPartPresentConditionComponent, SurgeryValidEvent>(OnPartPresentConditionValid);
-        SubscribeLocalEvent<SurgeryTraumaPresentConditionComponent, SurgeryValidEvent>(OnTraumaPresentConditionValid);
         SubscribeLocalEvent<SurgeryMarkingConditionComponent, SurgeryValidEvent>(OnMarkingPresentValid);
         //SubscribeLocalEvent<SurgeryRemoveLarvaComponent, SurgeryCompletedEvent>(OnRemoveLarva);
 
@@ -119,10 +112,8 @@ public abstract partial class SharedSurgerySystem : EntitySystem
     {
         if (!TryComp(args.Body, out DamageableComponent? damageable)
             || !TryComp(args.Part, out DamageableComponent? partDamageable)
-            || !TryComp(args.Part, out WoundableComponent? partWoundable)
             || damageable.TotalDamage <= 0
             && partDamageable.TotalDamage <= 0
-            && partWoundable.Wounds!.Count == 0
             && !HasComp<IncisionOpenComponent>(args.Part))
             args.Cancelled = true;
     }
@@ -213,12 +204,6 @@ public abstract partial class SharedSurgerySystem : EntitySystem
     {
         if (args.Part == EntityUid.Invalid
             || !HasComp<BodyPartComponent>(args.Part))
-            args.Cancelled = true;
-    }
-
-    private void OnTraumaPresentConditionValid(Entity<SurgeryTraumaPresentConditionComponent> ent, ref SurgeryValidEvent args)
-    {
-        if (!_trauma.HasWoundableTrauma(args.Part, ent.Comp.TraumaType))
             args.Cancelled = true;
     }
 
